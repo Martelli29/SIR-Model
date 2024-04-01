4dadff0bf66437d8c0823b7b86db4a7a21823d18
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2024-04-01 02:49:16 +0200

    input.py: implemented a function that raise error in case of wrong input values

--- input.py
+++ input.py
@@ -1,21 +1,38 @@
 
 def SetPar():
-    beta = float(input("Put in the infection probability (beta):\n"))
+    
     gamma = float(input("Put in the healing probability (gamma):\n"))
-    return beta, gamma
+    beta = float(input("Put in the infection probability (beta):\n"))
+    
+    return gamma, beta
 
 def SetT():
     
-    t= int(input("Put in the duration of the simulation (days):\n"))
+    t= int(input("Put in the duration of the simulation (days) (must be an integer!!!):\n"))
+    
     return t
 
 def SetSIR():
-    s = (int, input("Put in the numbers of susceptible:\n"))
-    i = (int, input("Put in the numbers of infectious:\n"))
-    r = (int, input("Put in the numbers of recovered:\n"))
+    
+    s = int(input("Put in the numbers of susceptible (must be an integer!!!):\n"))
+    i = int(input("Put in the numbers of infectious (must be an integer!!!):\n"))
+    r = int(input("Put in the numbers of recovered (must be an integer!!!):\n"))
+    
     return s, i, r
 
-if __name__=="__main__":
-    SetPar()
-    SetT()
-    SetSIR()
+def Inspector(gamma, beta, t,s,i,r):
+    
+    if t <= 0:
+        raise ValueError("Simulation cannot take place with times less than or equal to zero.")
+    
+    elif beta < 0 or beta > 1:
+        raise ValueError("Beta parameter must be between zero and one.")
+
+    elif gamma < 0 or gamma > 1:
+        raise ValueError("Gamma parameter must be between zero and one.")
+
+    elif s < 0 or i < 0 or r < 0:
+        raise ValueError("We can't have neagtive number of subsceptible, infected or recovered people.")
+    
+    elif s+i+r == 0:
+        raise ValueError("Population must be greater than zero!!!")
