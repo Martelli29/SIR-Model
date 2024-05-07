a9f586444ac3b364d399c1ff1b2c4f9ff5867738
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2024-05-07 15:17:47 +0200

    better name for a parameter: t -> vaccine_day

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot(S, I, R, t):
+def plot(S, I, R, vaccine_day):
     '''
     This function takes as input three lists and uses the functionality of matplotlib
     to plot them in a graph.
@@ -10,8 +10,8 @@ def plot(S, I, R, t):
     plt.plot(I, color="red")
     plt.plot(R, color="black")
 
-    if t != None:
-        plt.axvline(t, color='gray', linestyle='dashed', linewidth=1)
+    if vaccine_day != None:
+        plt.axvline(vaccine_day, color='gray', linestyle='dashed', linewidth=1)
     else:
         pass
     
