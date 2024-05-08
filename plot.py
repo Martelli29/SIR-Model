4f480ad41450afd2c07340534d3297bca0e3bf47
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2024-05-08 17:13:34 +0200

    better docstring for plot()

--- plot.py
+++ plot.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
 
+
 def plot(S, I, R, vaccine_day):
     '''
-    This function takes as input three lists and uses the functionality of matplotlib
-    to plot them in a graph.
+    This function is used for plotting the trend of the epidemic in a graph through
+    matplotlib library.
+
+    4 parameters needed:
+    S (list): contains the number of subsceptible in the population for each day in a sequential order.
+    I (list): contains the number of infected in the population for each day in a sequential order.
+    R (list): contains the number of removed in the population for each day in a sequential order.
+    vaccine_day (int): day on which contromeasures were taken.
     '''
-    
+
     plt.plot(S, color="blue")
     plt.plot(I, color="red")
     plt.plot(R, color="black")
@@ -14,12 +21,12 @@ def plot(S, I, R, vaccine_day):
         plt.axvline(vaccine_day, color='gray', linestyle='dashed', linewidth=1)
     else:
         pass
-    
+
     plt.xlabel("Days")
     plt.ylabel("Population")
     plt.grid(True)
-    
+
     plt.title("SIR model")
     plt.legend(["Subsceptible", "Infected", "Removed"])
-    
+
     plt.show()
